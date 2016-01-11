c519e90de437e7656bcfb1d64a9ba716a970dc44
Alexander Trost <Galexrt@googlemail.com>
2016-01-11 09:30:49 +0100

    Fix for the config modification

--- docker-start.sh
+++ docker-start.sh
@@ -4,28 +4,17 @@ DB_ADAPTER="${DB_ADAPTER:-mysql2}"
 DB_POOL="${DB_POOL:-5}"
 DB_HOST="${DB_HOST:-127.0.0.1}"
 DB_USER="${DB_USER:-staytus}"
+DB_PASSWORD="${DB_PASSWORD:-staytus}"
 DB_DATABASE="${DB_DATABASE:-staytus}"
 
 cp -f /opt/staytus/config/database.example.yml /opt/staytus/config/database.yml
 
-if [ ! -z "$DB_ADAPTER" ]; then
-    sed -i "s/adapter:.*/adapter: \"$DB_ADAPTER\"/" /opt/staytus/config/database.yml
-fi
-if [ ! -z "$DB_POOL" ]; then
-    sed -i "s|pool:.*|pool: $DB_POOL|" /opt/staytus/config/database.yml
-fi
-if [ -z "$DB_HOST" ]; then
-    sed -i "s|host:.*|host: \"$DB_HOST\"|" /opt/staytus/config/database.yml
-fi
-if [ ! -z "$DB_USER" ]; then
-    sed -i "s/username:.*/username: \"$DB_USER\"/" /opt/staytus/config/database.yml
-fi
-if [ ! -z "$DB_PASSWORD" ]; then
-    sed -i "s|password:.*|password: \"$DB_PASSWORD\"|" /opt/staytus/config/database.yml
-fi
-if [ ! -z "$DB_DATABASE" ]; then
-    sed -i "s|database:.*|database: $DB_DATABASE|" /opt/staytus/config/database.yml
-fi
+sed -i "s/adapter:.*/adapter: \"$DB_ADAPTER\"/" /opt/staytus/config/database.yml
+sed -i "s|pool:.*|pool: $DB_POOL|" /opt/staytus/config/database.yml
+sed -i "s|host:.*|host: \"$DB_HOST\"|" /opt/staytus/config/database.yml
+sed -i "s/username:.*/username: \"$DB_USER\"/" /opt/staytus/config/database.yml
+sed -i "s|password:.*|password: \"$DB_PASSWORD\"|" /opt/staytus/config/database.yml
+sed -i "s|database:.*|database: $DB_DATABASE|" /opt/staytus/config/database.yml
 
 cd /opt/staytus || exit 1
 
